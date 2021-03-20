a0a4ebd6857df7677c763e66b7398320093592d1
aadeeteeya <aadeeteeya99@gmail.com>
2021-03-20 10:59:51 +0530

    sad

--- webledger/settings.py
+++ webledger/settings.py
@@ -78,8 +78,7 @@ WSGI_APPLICATION = 'webledger.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default' : dj_database_url.config(
-        default=config('DATABASE_URL')
+    'default' : config('DATABASE_URL')
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
