e13ed813933679339f6d0920299acf629df0600a
shlok <shlok.dubey1@gmail.com>
2021-04-02 00:59:35 +0530

    Added staticfiles

--- webledger/settings.py
+++ webledger/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,13 +78,16 @@ WSGI_APPLICATION = 'webledger.wsgi.application'
 
 # Database for local development
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+#
 # DATABASES = {
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.sqlite3',
-#     #     'NAME': BASE_DIR / 'db.sqlite3',
-#     # }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+#  Heroku Free Database
 
 DATABASES = {
     'default': dj_database_url.config(
